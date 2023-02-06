73caf1c49e5ad1b971a9b83d00c12facddce5f8c
SILENT <110650499+SILENT-XD@users.noreply.github.com>
2023-02-06 15:43:42 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -8,15 +8,10 @@ except KeyError:
 	
 if __name__=='__main__':
 	try:os.mkdir('git pull')
-	except:pass
 	try:os.mkdir('OK')
-	except:pass
 	try:os.mkdir('CP')
-	except:pass
 	try:os.mkdir('/sdcard/OK')
-	except:pass
 	try:os.mkdir('/sdcard/CP')
-	except:pass
 	if "Indonesia" == fc:
 		__import__("silent").menu()
 	else:
